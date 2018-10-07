7e77dcce2255882ec3dab990841e18bc574007ff
root <root@ansible.localdomain>
2018-10-07 11:29:33 +0300

    it works

--- parser.py
+++ parser.py
@@ -1,15 +1,13 @@
 import re
 import sys
 from decimal import *
+from operator import itemgetter
 
 def index2d(myList, v):
     for i, x in enumerate(myList):
         if v in x:
             return (i, x.index(v))
 
-sumtime=0
-urls_and_time={}
-urls_and_count={}
 urls=[]
 for line in sys.stdin:
     result=re.split(r' ',line)
@@ -18,21 +16,16 @@ for line in sys.stdin:
     time=Decimal(re.sub('[\[\]\n]','',time_with_brackets))
     modified_path=re.sub('\d{1,}','%d',path)
     if any(modified_path in lines for lines in urls):
-        row_where_found_url=index2d(urls, modified_path)[0]
-        urls[row_where_found_url][1]+=time
-        urls[row_where_found_url][2]+=1
+        nrow_url=index2d(urls, modified_path)[0]
+        urls[nrow_url][1]+=time
+        urls[nrow_url][2]+=1
+        urls[nrow_url][3]=round(urls[nrow_url][1]/urls[nrow_url][2],3)
     else:    
-        urls.append([modified_path, time,1])
+        urls.append([modified_path, time,1,time])
 
-    if modified_path in urls_and_time: 
-        urls_and_time[modified_path]+=time
-        urls_and_count[modified_path]+=1
-    else:
-        urls_and_time[modified_path]=time
-        urls_and_count[modified_path]=1
-    #print (modified_path," ",time) 
-print (urls)
-#print (enumerate(urls))
-#print (urls.index(modified_path))
-print (urls_and_count)
-print (urls_and_time)    
+sorted_urls=sorted(urls, key=itemgetter(3), reverse=True)
+print (sorted_urls)
+for rows in sorted_urls:
+    for cells in rows:    
+        print (cells,'\t\t',end='')
+    print ('')
